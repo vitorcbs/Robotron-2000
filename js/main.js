39d79ce44f78b449d2776c06a7f241f796465049
Vitinho <vitorcbs18@gmail.com>
2022-12-30 17:06:46 -0300

    ex4 and data-attribute

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-const robotron = document.querySelector('#robotron')
+const controle = document.querySelectorAll("[data-controle]"); //QuerySelectorAll busca todos elementos da classe
 
-robotron.addEventListener('click', () => {
-    console.log('Clicou no robo')
-})
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(
+      evento.target.dataset.controle,
+      evento.target.parentNode
+    ); /*parent node pega o pai do elemento, entao aqui ta passando a opercao que vai realizar e o pai do local da operacao*/
+  });
+});
 
-function dizOi() {
-    console.log('Oi')
-    console.log('Bem vindo ao Robotron 2000')
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]");
+
+  if (operacao === "-") {
+    peca.value = parseInt(peca.value) - 1; //ParseInt transforma em numero
+  } else {
+    peca.value = parseInt(peca.value) + 1;
+  }
 }
-
-dizOi()
